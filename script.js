1981724008af1365205b8402e918a31dc4f124e9
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-02-26 17:33:01 -0500

    updated files

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let a = moment().format("A");
 let row = document.querySelectorAll(".row");
 let array = [];
 let save = document.querySelectorAll(".saveBtn");
+let today = document.querySelector("#currentDay");
+today.innerHTML = moment().format("dddd, MMMM Do");
 
-//Creates an array of text elements that contains the times of each row
+//Populates an array with text elements of the times of each row
 for (i = 0; i < row.length; i++) {
     let time = row[i].children[0];
     array.push(time);
@@ -15,18 +17,20 @@ for (i = 0; i < row.length; i++) {
 //present or future
 for (i = 0; i < array.length; i++) {
     let text = array[i].innerHTML;
+    let textarea = array[i].parentNode.children[1];
+
     if (a[0] === text[text.length - 2]) {
         if (parseInt(text) % 12 > h % 12) {
-            array[i].parentNode.children[1].classList.add("future");
+            textarea.classList.add("future");
         } else if (parseInt(text) % 12 < h % 12) {
-            array[i].parentNode.children[1].classList.add("past");
+            textarea.classList.add("past");
         } else {
-            array[i].parentNode.children[1].classList.add("present");
+            textarea.classList.add("present");
         }
     } else if (a === "AM") {
-        array[i].parentNode.children[1].classList.add("future");
+        textarea.classList.add("future");
     } else {
-        array[i].parentNode.children[1].classList.add("past");
+        textarea.classList.add("past");
     }
 }
 
@@ -39,6 +43,5 @@ for (i = 0; i < save.length; i++) {
     save[i].addEventListener("click", function() {    
         date.value = this.parentNode.children[1].value;
         localStorage.setItem(time, date.value);
-        console.log(time + ", " + date.value);
     })
 }
