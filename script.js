7854424a234145639817cd7275ec2d0688a15e97
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-03-04 02:36:24 -0500

    initial commit for homework 6

--- script.js
+++ script.js
@@ -1,47 +1,101 @@
 //Global variables defined
-let h = moment().format("h");
-let a = moment().format("A");
-let row = document.querySelectorAll(".row");
-let array = [];
-let save = document.querySelectorAll(".saveBtn");
-let today = document.querySelector("#currentDay");
-today.innerHTML = moment().format("dddd, MMMM Do");
+let input = document.querySelector(".input");
+let button = document.querySelector("button");
+let search = document.querySelector("#search");
+let current = document.querySelector("#current");
+let week = document.querySelector("#week");
 
-//Populates an array with text elements of the times of each row
-for (i = 0; i < row.length; i++) {
-    let time = row[i].children[0];
-    array.push(time);
-}
+//Function that populates the search history
+button.addEventListener("click", function() {
+    let history = document.createElement("button");
+    history.innerHTML = input.value;
+    search.appendChild(history);
 
-//Loops through the hours in the working day and color codes them based on whether they are in the past,
-//present or future
-for (i = 0; i < array.length; i++) {
-    let text = array[i].innerHTML;
-    let textarea = array[i].parentNode.children[1];
-
-    if (a[0] === text[text.length - 2]) {
-        if (parseInt(text) % 12 > h % 12) {
-            textarea.classList.add("future");
-        } else if (parseInt(text) % 12 < h % 12) {
-            textarea.classList.add("past");
-        } else {
-            textarea.classList.add("present");
-        }
-    } else if (a === "AM") {
-        textarea.classList.add("future");
-    } else {
-        textarea.classList.add("past");
-    }
-}
-
-//Creates an event listener for the save button of each hour and saves inputed text into local storage
-for (i = 0; i < save.length; i++) {
-    let time = save[i].parentNode.children[0].innerHTML;
-    let date = save[i].parentNode.children[1];
-    date.value = localStorage.getItem(time);
-
-    save[i].addEventListener("click", function() {    
-        date.value = this.parentNode.children[1].value;
-        localStorage.setItem(time, date.value);
+    history.addEventListener("click", function() {
+        console.log(this.innerHTML);
     })
+    history.addEventListener("click", getCurrent);
+    history.addEventListener("click", getFiveDay);
+});
+
+//Function that populates a field with the current weather conditions of the desired city
+button.addEventListener("click", getCurrent);
+function getCurrent() {
+    let requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + input.value + "&appid=de53a40654766cb8ce20288a99c9f736";
+    console.log(requestUrl);
+
+    fetch(requestUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+
+        let tempF = Math.floor((data["main"]["temp"] - 273.15) * 9/5 + 32);
+
+        current.children[0].innerHTML = data["name"] + moment().format(" (M/D/YYYY) ");
+
+        let image = document.createElement("img");
+        image.setAttribute("src", "http://openweathermap.org/img/wn/" + data["weather"][0]["icon"] + "@2x.png");
+        current.children[0].appendChild(image);
+
+        current.children[1].innerHTML = "Temperature: " + tempF + " F";
+        current.children[2].innerHTML = "Humidity: " + data["main"]["humidity"] + "%";
+        current.children[3].innerHTML = "Wind Speed: " + data["wind"].speed + " mph";
+
+        let lat = data["coord"]["lat"];
+        let lon = data["coord"]["lon"];
+        getFiveDay(lat, lon);
+        
+
+      })
+      .catch(function() {
+        console.log("Error");
+      });
 }
+
+//Function that accesses the UV Index and five day forecast
+function getFiveDay(lat, lon) {
+    let requestUrl = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=de53a40654766cb8ce20288a99c9f736";
+    console.log(requestUrl);
+
+    fetch(requestUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        current.children[4].innerHTML = "UV Index: " + data["current"]["uvi"];
+
+        for (i = 1; i <= 5; i++) {
+            let tempF = Math.floor((data["daily"][i]["temp"]["day"] - 273.15) * 9/5 + 32);
+            // let tempF = data["list"][i]["temp"]["day"]["imperial"];
+            let day = week.children[i - 1];
+            let dd = parseInt(moment().format("d")) + i;
+            console.log(dd);
+            day.innerHTML = "";
+
+            let date = document.createElement("p");
+            date.innerHTML = data["daily"][i]["dt"];
+            date.innerHTML = moment().format("M/" + dd + "/YYYY");
+            day.appendChild(date);
+
+            let emoji = document.createElement("img");
+            emoji.setAttribute("src", "http://openweathermap.org/img/wn/" + data["daily"][i]["weather"][0]["icon"] + "@2x.png");
+            day.appendChild(emoji);
+
+            let temp = document.createElement("p");
+            temp.innerHTML = "Temp: " + tempF + " F";
+            day.appendChild(temp);
+
+            let humid = document.createElement("p");
+            humid.innerHTML = "Humidity: " + data["daily"][i]["humidity"] + "%";
+            day.appendChild(humid);
+        }
+
+      })
+      .catch(function() {
+        console.log("Error");
+      });
+}
+
